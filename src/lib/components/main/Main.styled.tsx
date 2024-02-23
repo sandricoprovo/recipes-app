7995f46579eca4f7323a3e5c4a097ff913d901d6
Sandrico Provo <Sprovo@outlook.com>
2024-02-23 17:27:01 -0400

    [feat] adds recipe info view ui

--- src/lib/components/main/Main.styled.tsx
+++ src/lib/components/main/Main.styled.tsx
@@ -3,6 +3,8 @@
 import styled from 'styled-components';
 
 export const MainStyled = styled.main`
+    position: relative;
+
     display: flex;
     flex-direction: column;
     justify-content: center;
